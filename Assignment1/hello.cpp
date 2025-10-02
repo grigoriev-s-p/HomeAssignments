5b595736d30fffee2e3fe121b7205c87b7d605ed
Sergey <st142081@student.spbu.ru>
2025-10-02 17:16:29 +0300

    create directory

---
+++ Assignment1/hello.cpp
@@ -0,0 +1,10 @@
+#include "hello.h"
+#include <iostream>
+void myproject::sayHello() {
+	std::cout << "Hello, world!" << std::endl;
+	while(true){
+		std::string s;
+		std::cin>>s;
+		std::cout<<"Hello, "<<s<<"!"<<std::endl;
+	}
+}
