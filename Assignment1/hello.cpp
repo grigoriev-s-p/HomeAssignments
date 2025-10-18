cd76fac49224fbf83f12ea3202ebc127da15c829
Sergey <st142081@student.spbu.ru>
2025-10-18 13:19:08 +0300

    merge directories

--- Assignment1/hello.cpp
+++ Assignment1/hello.cpp
@@ -9,7 +9,7 @@ void myproject::sayHello() {
 	std::cout << "Hello, world!" << std::endl;
 	while(true){
 		std::string s;
-		std::cin>>s;
+		std::getline(std::cin,s);
 		std::cout<<"Hello, "<<s<<"!"<<std::endl;
 	}
 }
